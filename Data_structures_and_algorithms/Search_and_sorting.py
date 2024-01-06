74e732c5242a76c13b531704f36e91bb824791c0
Taehyun Sung <alexseong1004@gmail.com>
2024-01-07 00:03:36 +0800

    Q1 in Search and sorting

---
+++ Data_structures_and_algorithms/Search_and_sorting.py
@@ -0,0 +1,28 @@
+"""
+# 1
+Write a Python program for binary search.
+Binary Search : In computer science, a binary search or half-interval search algorithm finds the position of a target value within a sorted array. The binary search algorithm can be classified as a dichotomies divide-and-conquer search algorithm and executes in logarithmic time.
+Test Data :
+binary_search([1,2,3,5,8], 6) -> False
+binary_search([1,2,3,5,8], 5) -> True
+"""
+
+
+def binary_search(array, search_value):
+    sorted_array = sorted(array)
+    first = 0
+    last = len(array) - 1
+    found = False
+    while first <= last and not found:
+        mid = (first + last) // 2
+        if sorted_array[mid] == search_value:
+            found = True
+        else:
+            if search_value < sorted_array[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
+    return found
+
+
+print(binary_search([1, 2, 3, 5, 8, 10, 12, 40, 100], 1))
