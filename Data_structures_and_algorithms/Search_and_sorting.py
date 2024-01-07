8721333ee4c1d297065cb401f47fc8c6b4335253
Taehyun Sung <alexseong1004@gmail.com>
2024-01-07 23:38:22 +0800

    Q2 completed

--- Data_structures_and_algorithms/Search_and_sorting.py
+++ Data_structures_and_algorithms/Search_and_sorting.py
@@ -25,4 +25,22 @@ def binary_search(array, search_value):
     return found
 
 
-print(binary_search([1, 2, 3, 5, 8, 10, 12, 40, 100], 1))
+# print(binary_search([1, 2, 3, 5, 8, 10, 12, 40, 100], 1))
+
+
+"""
+# 2
+Write a Python program for sequential search.
+Sequential Search : In computer science, linear search or sequential search is a method for finding a particular value in a list that checks each element in sequence until the desired element is found or the list is exhausted. The list need not be ordered.
+Test Data :
+Sequential_Search([11,23,58,31,56,77,43,12,65,19],31) -> (True, 3)
+"""
+
+
+def sequential_search(array, search_value):
+    for x in range(len(array)):
+        if array[x] == search_value:
+            print(f"True, {x}")
+
+
+sequential_search([11,23,58,31,56,77,43,12,65,19],65)
